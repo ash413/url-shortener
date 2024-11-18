ada636c3cf771dc8a9af8a4299aa4c7ff815b6d6
ash413 <vaishnavi.kadam413@gmail.com>
2024-11-18 00:23:11 -0500

    setup mac

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const validUrl = require('valid-url')
+
 
 const app = express()
 
 app.use(express.json())
 
-app.get('', (req, res) => {
-    
+app.post('/', (req, res) => {
+    const { longUrl } = req.body;
+
+    if (!valid)
 })
 
-app.listen(3000)
+app.listen( process.env.PORT || 3000)
