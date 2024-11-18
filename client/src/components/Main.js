86a49bc240af1a7fb40ef35c10490f8314428994
ash413 <vaishnavi.kadam413@gmail.com>
2024-11-18 00:24:20 -0500

    client

---
+++ client/src/components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h1>URL Shortener app!</h1>
+                <p>Get rid of those long urls now!</p>
+            </div>
+            <div>
+                <form>
+                    <input>
+                        
+                    </input>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            an image or logo on the top left [TINYCLIP]
+        </div>
+        <div>
+            one-two links in the right 
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
